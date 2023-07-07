952620de5472a991ab39062980d65b50c262ecc4
sanafatmagithub <sanaafatma111@gmail.com>
2023-07-08 01:51:11 +0530

    demo1

--- dom.js
+++ dom.js
@@ -9,16 +9,34 @@
 // console.log(document.forms[0]);
 // console.log(document.links);
 //console.log(document.images);
+// GETELEMENTBYID
 // var headerTitle =console.log(document.getElementById("header-title"));
 // console.log(headerTitle);
 // headerTitle.textContent='Hello';
 //headerTitle.innerText='Goodbye';
+//console.log (headerTitle.innerText);
+//headerTitle.innerHTML='<h3>Hello</h3>;
+//header.style.borderBottom='solid 3px #000';
 
- let items =document.getElementsByClassName("list-group-item");
- console.log(items);
+//  GETELEMENTBYCLASSNAME//
+//  let items =document.getElementsByClassName("list-group-item");
+//  console.log(items);
+//  console.log(items[1]);
+// items[1].textContent='hello2'
+//  items[2].style.backgroundColor='green';
+//  for(let i=0; i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+//  }
 
- items[2].style.backgroundColor='green';
- for(let i=0; i<items.length;i++){
-    items[i].style.fontWeight='bold';
+//GETELEMENTBYTAGNAME//
+let li =document.getElementsByTagName("li");
+ console.log(li);
+ console.log(li[1]);
+ //li[1].textContent='hello2'
+ li[4].style.backgroundColor='green';
+ for(let i=0; i<li.length;i++){
+    li[i].style.fontWeight='bold';
  }
+
+ 
  
