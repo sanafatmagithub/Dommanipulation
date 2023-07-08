90c4dcf285dc29c471ba1e6a0da7eefb6220cdce
sanafatmagithub <sanaafatma111@gmail.com>
2023-07-08 14:13:15 +0530

    demo2

--- dom.js
+++ dom.js
@@ -29,13 +29,49 @@
 //  }
 
 //GETELEMENTBYTAGNAME//
-let li =document.getElementsByTagName("li");
- console.log(li);
- console.log(li[1]);
- //li[1].textContent='hello2'
- li[4].style.backgroundColor='green';
- for(let i=0; i<li.length;i++){
-    li[i].style.fontWeight='bold';
+// let li =document.getElementsByTagName("li");
+//  console.log(li);
+//  console.log(li[1]);
+//  //li[1].textContent='hello2'
+//  li[4].style.backgroundColor='green';
+//  for(let i=0; i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+//  }
+
+//QUERYSELECTOR//
+// let header =document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+// let input = document.querySelector('input');
+// input.value='Hello World';
+
+// let submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND";
+
+// let item = document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// let lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue';
+
+  let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+  secondItem.style.backgroundColor = 'green';
+
+  let thirdItem= document.querySelector('.list-group-item:nth-child(3)');
+  thirdItem.style.visibility = "hidden";
+
+
+// QUERYSELECTORALL //
+// let titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent='Hello';
+   let sItem = document.querySelectorAll('li')[1];
+   sItem.style.color= 'green';
+
+ let odd = document.querySelectorAll('li:nth-child(odd)');
+// let even = document.querySelectorAll('li:nth-child(even)');
+   for(let i=0; i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
+//    even[i].style.backgroundColor = '#ccc' ;
  }
 
  
